36aa1b5a6199b1e330495ad08f6d6f605ba9474b
얌챠 <dbals9812@naver.com>
2020-10-11 03:08:15 +0900

    Update VirtualEditor.cpp

--- VirtualEditor/VirtualEditor.cpp
+++ VirtualEditor/VirtualEditor.cpp
@@ -26,12 +26,26 @@ struct VirtualPass : public FunctionPass {
           // 할당할 변수들을 넣을 공간 생성
 
           IRBuilder<> builder(entry_bb);
-          Value *var_retval = builder.CreateAlloca(builder.getInt32Ty());
-          Value *var_i = builder.CreateAlloca(builder.getInt32Ty());
+          // llvm::Constant *value_4 =
+          // llvm::ConstantInt::get(Type::getInt32Ty(f.getContext()), 4, true);
+          // Value *var_retval = new
+          // AllocaInst(Type::getInt32Ty(f.getContext()),
+          //                                    NULL, value_4, "retval",
+          //                                    entry_bb);
+          // Value *var_i = new AllocaInst(Type::getInt32Ty(f.getContext()),
+          // NULL,
+          //                               value_4, "i", entry_bb);
+          Value *var_retval = builder.CreateAlloca(
+              Type::getInt32Ty(f.getContext()), nullptr, "retval");
+          Value *var_i = builder.CreateAlloca(Type::getInt32Ty(f.getContext()),
+                                              nullptr, "i");
+
           // retval, i에 해당하는 변수 할당
 
-          Value *value_0 = Constant::getNullValue(builder.getInt32Ty());
-          StoreInst *store_inst = new StoreInst(value_0, var_i, entry_bb);
+          Value *value_0 =
+              Constant::getNullValue(Type::getInt32Ty(f.getContext()));
+          // StoreInst *store_inst = new StoreInst(value_0, var_i, entry_bb);
+          builder.CreateStore(value_0, var_i, /*isVolatile=*/false);
           // i 변수에 0값 넣어줌
 
           BranchInst::Create(original_entry_bb, entry_bb);
@@ -64,11 +78,37 @@ struct VirtualPass : public FunctionPass {
               end_bb->splitBasicBlock(split_point_ret, "returnBB");
           // condBB, bodyBB, endBB, returnBB로 구분
 
+          // Value *lhs = new LoadInst(Type::getInt32Ty(f.getContext()), var_i, "",
+          //                           cond_bb);
+          IRBuilder<> builder_cond(cond_bb);
+          Value *lhs = builder_cond.CreateLoad(var_i);
+          // i값을 condBB에 load
+          // ICmpInst *cond_instruction =
+          //     new ICmpInst(cond_bb, ICmpInst::ICMP_SGE, lhs, value_0,"");
+          Value *cond_instruction = builder_cond.CreateICmpSGE(lhs, value_0, "");
+          BranchInst::Create(body_bb, end_bb, cond_instruction, cond_bb);
+          // i의 값에 따라 bodyBB와 endBB로 분기하는 분기문 생성
+          BasicBlock::iterator to_remove_cond = cond_bb->begin();
+          Instruction *inst_to_remove_cond = &(*to_remove_cond);
+          inst_to_remove_cond->dropAllReferences();
+          inst_to_remove_cond->eraseFromParent();
+          // condBB의 원래 분기문을 없애면 condBB부분 완성
+
+          IRBuilder<> builder_end(end_bb);
+          builder_end.CreateStore(value_0, var_retval, /*isVolatile=*/false);
+          builder_end.CreateBr(return_bb);
+          BasicBlock::iterator to_remove_end= end_bb->begin();
+          Instruction *inst_to_remove_end = &(*to_remove_end);
+          inst_to_remove_end->dropAllReferences();
+          inst_to_remove_end->eraseFromParent();
+          // endBB에 0대입하는 것 넣어주기
+
 
           entry_bb->dump();
           original_entry_bb->dump();
           functioning_bb->dump();
           return_bb->dump();
+          //프린트하는 부분
 
           // BinaryOperator::Create(Instruction::Add, op, rhs, "", nextInst);
           // BinaryOperator::Create(Instruction::Add, op, rhs, "", nextInst);
