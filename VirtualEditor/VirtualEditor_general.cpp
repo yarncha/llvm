e209446791fb29340e079fe8472d8786e729e60c
얌챠 <dbals9812@naver.com>
2020-10-30 18:05:49 +0900

    Create VirtualEditor_general.cpp

---
+++ VirtualEditor/VirtualEditor_general.cpp
@@ -0,0 +1,147 @@
+using namespace llvm;
+using namespace std;
+
+namespace {
+struct VirtualPass : public FunctionPass {
+  static char ID;
+  VirtualPass() : FunctionPass(ID) {}
+
+  // Used Functions S ----------
+  BasicBlock* splitBasicBlockWithNumberAndName(BasicBlock *target_basicblock, string new_basicblock_name, int number_to_move) {
+    BasicBlock::iterator split_point = (BasicBlock::iterator)target_basicblock->getFirstNonPHIOrDbgOrLifetime();
+    for (int counter = 0; counter < number_to_move; counter++) {
+      split_point++;
+    }
+    BasicBlock * splitted= target_basicblock->splitBasicBlock(split_point, new_basicblock_name);
+    return splitted;
+  }
+  // target_basicblock -> new_basicblock 구조로 만들어짐
+
+  void deleteInstructionWithPosition(BasicBlock *target_basicblock, int number_to_move) {
+    BasicBlock::iterator to_remove = target_basicblock->begin();
+    for (int counter = 0; counter < number_to_move; counter++) {
+      to_remove++;
+    }
+    Instruction *inst_to_remove = &(*to_remove);
+    inst_to_remove->dropAllReferences();
+    inst_to_remove->eraseFromParent();
+  }
+  // Used Functions E ----------
+
+  virtual bool runOnFunction(Function &f) {
+    errs() << "- Start of function [" << f.getName() << "]\n";
+
+    // main function이 아닌, adderObfuscated function에서만 난독화 진행
+    if (f.getName() != "main") {
+      for (BasicBlock &bb : f) {
+
+        // entry bb에서 난독화를 진행하도록
+        if (bb.getName() == "entry") {
+          BasicBlock *entry_bb = &bb; // BasicBlock 클래스를 사용하기 위해 바꿔줌
+
+
+          BasicBlock *original_entry_bb = splitBasicBlockWithNumberAndName(entry_bb,"originEntry",0);
+          IRBuilder<> builder(entry_bb);
+          Value *var_retval = builder.CreateAlloca(Type::getInt32Ty(f.getContext()), nullptr, "retval");
+          Value *var_i = builder.CreateAlloca(Type::getInt32Ty(f.getContext()), nullptr, "i");
+          // entry_bb에 retval, i에 해당하는 변수 할당
+          Value *value_0 = Constant::getNullValue(Type::getInt32Ty(f.getContext()));
+          builder.CreateStore(value_0, var_i, false);
+          // i 변수에 0값 넣어줌
+          BranchInst::Create(original_entry_bb, entry_bb);
+          deleteInstructionWithPosition(entry_bb,0);
+          // entry_bb 완성
+
+
+          BasicBlock *cond_bb = splitBasicBlockWithNumberAndName(original_entry_bb,"condBB",6);
+          BasicBlock *end_bb = splitBasicBlockWithNumberAndName(cond_bb,"endBB",8);
+          BasicBlock *body_bb = splitBasicBlockWithNumberAndName(cond_bb,"bodyBB",0);
+          BasicBlock *return_bb = splitBasicBlockWithNumberAndName(end_bb,"returnBB",0);
+          // condBB, bodyBB, endBB, returnBB로 나누기
+
+
+          IRBuilder<> builder_cond(cond_bb);
+          Value *lhs = builder_cond.CreateLoad(var_i);
+          // i값을 condBB에 load
+          Value *cond_instruction = builder_cond.CreateICmpSGE(lhs, value_0, "");
+          BranchInst::Create(body_bb, end_bb, cond_instruction, cond_bb);
+          // i의 값에 따라 bodyBB와 endBB로 분기하는 분기문 생성
+          deleteInstructionWithPosition(cond_bb, 0);
+          // condBB의 원래 분기문을 없애면 condBB부분 완성
+
+          IRBuilder<> builder_end(end_bb);
+          builder_end.CreateStore(value_0, var_retval, false);
+          // endBB에 0대입하는 것 넣어주기
+          builder_end.CreateBr(return_bb);
+          deleteInstructionWithPosition(end_bb, 0);
+          // endBB부분 완성
+
+          IRBuilder<> builder_ret(return_bb);
+          Value *var_to_ret = builder_ret.CreateLoad(var_retval);
+          Value *return_inst = builder_ret.CreateRet(var_to_ret);
+          deleteInstructionWithPosition(return_bb, 0);
+          // returnBB에서 retval을 리턴할 수 있도록 변경
+
+          BasicBlock *case_bb = splitBasicBlockWithNumberAndName(body_bb,"caseBB",0);
+          IRBuilder<> builder_body(body_bb);
+          Value *var_switch = builder_body.CreateLoad(var_i);
+          deleteInstructionWithPosition(body_bb, 0);
+          //switch문을 넣을 body_bb
+
+          BasicBlock *sw_epliog_bb = BasicBlock::Create(f.getContext(), "epliogBB", &f, end_bb);
+          IRBuilder<> builder_epliog(sw_epliog_bb);
+          builder_epliog.CreateBr(cond_bb);
+          // 에필로그 블럭 생성
+
+          SwitchInst *switch_inst = builder_body.CreateSwitch(var_switch, sw_epliog_bb, 10);
+          // 스위치문 생성
+
+          //---테스트 완료---
+
+          BasicBlock *case_2_bb = splitBasicBlockWithNumberAndName(case_bb,"case2BB",4);
+          BasicBlock *case_3_bb = splitBasicBlockWithNumberAndName(case_2_bb,"case3BB",3);
+          //case 세 개로 나누기
+
+          IRBuilder<> builder_case(case_bb);
+          deleteInstructionWithPosition(case_bb, (case_bb->size())-1);
+          Value *value_1 = builder_case.getInt32(1);
+          builder_case.CreateStore(value_1, var_i, false);
+          builder_case.CreateBr(sw_epliog_bb);
+
+          IRBuilder<> builder_case_2(case_2_bb);
+          deleteInstructionWithPosition(case_2_bb, (case_2_bb->size())-1);
+          Value *value_2 = builder_case.getInt32(2);
+          builder_case_2.CreateStore(value_2, var_i, false);
+          builder_case_2.CreateBr(sw_epliog_bb);
+
+          IRBuilder<> builder_case_3(case_3_bb);
+          deleteInstructionWithPosition(case_3_bb, (case_3_bb->size())-1);
+          Value *loaded_data = &(*(case_3_bb->begin()));
+          builder_case_3.CreateStore(loaded_data, var_retval, false);
+          builder_case_3.CreateBr(return_bb);
+          //각 case에 대한 작업
+
+          switch_inst->addCase(builder_body.getInt32(0),case_bb);
+          switch_inst->addCase(builder_body.getInt32(1),case_2_bb);
+          switch_inst->addCase(builder_body.getInt32(2),case_3_bb);
+          //addcase
+
+          entry_bb->dump();
+          original_entry_bb->dump();
+          cond_bb->dump();
+          body_bb->dump();
+          case_bb->dump();
+          case_2_bb->dump();
+          case_3_bb->dump();
+          sw_epliog_bb->dump();
+          end_bb->dump();
+          return_bb->dump();
+          //프린트하는 부분
+        }
+      }
+      return false;
+    }
+  }
+};
+} // namespace
+
